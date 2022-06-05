64884b91acfbfd0f0511f98eebe7a47245641a2a
Your Name <you@example.com>
2022-06-05 10:58:14 -0600

    especialidades

---
+++ GUI/panelEspecialidades.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Feria Virtual</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  </head>
+
+  <header>
+      <div class="container">
+          <div class="row">
+            <a class=" pl-5 navbar-brand" href="#inicio">
+                <img src="./img/covaito2.png" class="img-fluid" alt="...">
+            </a>
+          </div>
+      </div>
+  </header>
+  <hr style="border: 5px solid #c0c0c060;" class="mt-12">
+
+  <body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 mx-auto text-center">
+                <h1>Especialidades</h1>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="container">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                <img src="./img/Informatica.jpg" style="width: 150px; height: 150px;" class="card-img-top rounded rounded mx-auto d-block" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Informática en DW</h5>
+                        <p class="card-text">Administrar el contenido que se muestra: videos, enlaces e información.</p>
+                        <a href="#" class="btn btn-primary">Ingresar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                <img src="./img/conta.png" style="width: 150px; height: 150px;" class="card-img-top rounded rounded mx-auto d-block" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Contabilidad y Finanzas</h5>
+                        <p class="card-text">Administrar el contenido que se muestra: videos, enlaces e información.</p>
+                        <a href="#" class="btn btn-primary">Ingresar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                <img src="./img/diseño.png" style="width: 150px; height: 150px;" class="card-img-top rounded rounded mx-auto d-block" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Diseño Publicitario</h5>
+                        <p class="card-text">Administrar el contenido que se muestra: videos, enlaces e información.</p>
+                        <a href="#" class="btn btn-primary">Ingresar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                <img src="./img/electronica.png" style="width: 150px; height: 150px;" class="card-img-top rounded rounded mx-auto d-block" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Electrónica Industrial</h5>
+                        <p class="card-text">Administrar el contenido que se muestra: videos, enlaces e información.</p>
+                        <a href="#" class="btn btn-primary">Ingresar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                <img src="./img/mante.png" style="width: 150px; height: 150px;" class="card-img-top rounded rounded mx-auto d-block" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Mantenimiento Industrial</h5>
+                        <p class="card-text">Administrar el contenido que se muestra: videos, enlaces e información.</p>
+                        <a href="#" class="btn btn-primary">Ingresar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                <img src="./img/Accounting.jpg" style="width: 150px; height: 150px;" class="card-img-top rounded rounded mx-auto d-block" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Accounting</h5>
+                        <p class="card-text">Administrar el contenido que se muestra: videos, enlaces e información.</p>
+                        <a href="#" class="btn btn-primary">Ingresar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                <img src="./img/automotriz.png" style="width: 150px; height: 150px;" class="card-img-top rounded rounded mx-auto d-block" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Mecánica Automotriz</h5>
+                        <p class="card-text">Administrar el contenido que se muestra: videos, enlaces e información.</p>
+                        <a href="#" class="btn btn-primary">Ingresar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                <img src="./img/bilingualSecretary.png" style="width: 150px; height: 150px;" class="card-img-top rounded rounded mx-auto d-block" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Bilingual Secretary</h5>
+                        <p class="card-text">Administrar el contenido que se muestra: videos, enlaces e información.</p>
+                        <a href="#" class="btn btn-primary">Ingresar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                <img src="./img/mecanicaPrecision.png" style="width: 150px; height: 150px;" class="card-img-top rounded rounded mx-auto d-block" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Mecánica de Precisión</h5>
+                        <p class="card-text">Administrar el contenido que se muestra: videos, enlaces e información.</p>
+                        <a href="#" class="btn btn-primary">Ingresar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <br>
+    <hr style="border: 5px solid #c0c0c060;" class="mt-12">
+    <footer>
+        <div class="col-md-12 mx-auto text-center">
+            <p class="pl-5 text-right">Hospício de Huérfanos de Cartago ©2020-2022</p>
+            <p class="pl-5 text-right">IDS 2022</p>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  </body>
+</html>
