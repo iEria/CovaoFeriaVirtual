6d87faafa38c2a647de0f2160de13e5a23a335ce
Your Name <you@example.com>
2022-06-05 15:30:23 -0600

    a

--- GUI/index.php
+++ GUI/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Feria Virtual</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="./css/Styles.css">
   </head>
   <header>
       <div class="container">
@@ -26,7 +27,7 @@
                     <p class="text-center">Feria Vocacional 2022</p>
                     <p class="text-center">¿Estás listo para triunfar?</p>
                     <div class="col-md-12 mx-auto text-center">
-                      <a href="avatar.php" class="btn btn-primary"">INICIAR</a>
+                    <a href="avatar.php" class="btn btn-primary"">INICIAR</a>
                     </div>
                 </div>
             </div>
