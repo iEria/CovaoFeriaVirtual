dd8d61b3a66d6c0ea825ff34650078e90f1b0520
Your Name <you@example.com>
2022-06-05 11:55:46 -0600

    foots and heads

--- GUI/mante.php
+++ GUI/mante.php
@@ -6,8 +6,26 @@
     <title>Feria Vritual</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   </head>
+  <header>
+      <div class="container">
+          <div class="row">
+            <a class=" pl-5 navbar-brand" href="#inicio">
+                <img src="./img/covaito2.png" class="img-fluid" alt="...">
+            </a>
+          </div>
+      </div>
+  </header>
+
   <body>
     <h1>Hello, world!</h1>
+    
+    <footer>
+        <div class="col-md-12 mx-auto text-center">
+            <p class="pl-5 text-right">Hospício de Huérfanos de Cartago ©2020-2022</p>
+            <p class="pl-5 text-right">IDS 2022</p>
+        </div>
+    </footer>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
   </body>
 </html>
