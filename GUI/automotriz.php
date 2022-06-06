27cf9329660e169874ea4a0b1f3b94420e3d7726
Your Name <you@example.com>
2022-06-06 07:44:13 -0600

    navbar

--- GUI/automotriz.php
+++ GUI/automotriz.php
@@ -9,12 +9,54 @@
   <header style="background-color: #27A4D0;">
       <div class="container">
           <div class="row">
-            <a class=" pl-5 navbar-brand" href="#inicio">
-                <img src="./img/covaito2.png" class="img-fluid" alt="...">
-            </a>
+          <nav class="navbar fixed-top" style="background-color: #27A4D0;">
+            <div class="container-fluid">
+                <a class=" pl-5 navbar-brand" href="#">
+                    <img src="./img/covaito2.png" class="img-fluid" alt="...">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                    <div class="offcanvas-header">
+                        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="informatica.php">Informática en Desarrollo Web</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="contabilidad.php">Contabilidad y Finanzas</a>
+                    </li>
+                        <a class="nav-link active" aria-current="page" href="diseno.php">Diseño Publicitario</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="electro.php">Electrónica Industrial</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="mante.php">Mantenimiento Industrial</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="accounting.php">Accounting</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="automotriz.php">Mecánica Automotriz</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="secretariado.php">Bilingual Secretary</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="precision.php">Mecánica de Precisión</a>
+                    </li>
+                    </ul>
+                </div>
+            </div>
+            </div>
+            </nav>
           </div>
       </div>
-      <hr style="border: 5px solid #0F4E9E;" class="mt-12">
   </header>
 
   <body>
