d066e1c1321523366f3f5c1aa1cf2b44ad8bcc73
Your Name <you@example.com>
2022-06-05 20:30:45 -0600

    swith

--- GUI/videosAccounting.php
+++ GUI/videosAccounting.php
@@ -32,24 +32,36 @@
         <div class="row row-cols-1 row-cols-md-2 g-4">
             <div class="col">
                 <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Video 1</h5>
-                    <p class="card-text">//Descripción del video</p>
-                </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Video 1</h5>
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                            <label class="form-check-label" for="flexSwitchCheckDefault">Activado</label>
+                        </div>
+                        <p class="card-text">//Descripción del video</p>
+                    </div>
                 </div>
             </div>
             <div class="col">
                 <div class="card">
-                <div class="card-body">
-                <h5 class="card-title">Video 2</h5>
-                    <p class="card-text">//Descripción del video</p>
-                </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Video 2</h5>
+                        <div class="form-check form-switch">
+                                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                                <label class="form-check-label" for="flexSwitchCheckDefault">Activado</label>
+                        </div>
+                        <p class="card-text">//Descripción del video</p>
+                    </div>
                 </div>
             </div>
             <div class="col">
                 <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Video 3</h5>
+                    <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                            <label class="form-check-label" for="flexSwitchCheckDefault">Activado</label>
+                    </div>
                     <p class="card-text">//Descripción del video</p>
                 </div>
                 </div>
@@ -58,6 +70,10 @@
                 <div class="card">
                 <div class="card-body">
                 <h5 class="card-title">Video 4</h5>
+                <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                            <label class="form-check-label" for="flexSwitchCheckDefault">Activado</label>
+                        </div>
                     <p class="card-text">//Descripción del video</p>
                 </div>
                 </div>
